a93f30b2eef88b2f45e4133be73e9353fc436a2d
Emma Lin <e33lin@uwaterloo.ca>
2021-02-07 01:52:01 +0000

    added response table

---
+++ db/migrate/20210207014936_create_responses.rb
@@ -0,0 +1,11 @@
+class CreateResponses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :responses do |t|
+      t.integer :survey_id, null:false
+      t.integer :question_number, null:false
+      t.integer :response, null:false
+
+      t.timestamps
+    end
+  end
+end
