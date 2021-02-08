2d08fa2ef5d4e7f5f37e75ec4ff91749be605aeb
Thomas Mastantuono <tmastant@uwaterloo.ca>
2021-02-08 17:35:27 +0000

    add password to users datatable

--- db/migrate/20210208012207_create_users.rb
+++ db/migrate/20210208012207_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :watiam, null: false, limit: 50
       t.string :user_id, null: false, limit: 50
+      t.string :password, null: false, limit: 100
       t.string :first_name, null: false, limit: 50
       t.string :last_name, null: false, limit: 50
 
