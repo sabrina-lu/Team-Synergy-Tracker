d54faaa75deae1953ee152a02fa5b7d8f219642b
Thomas Mastantuono <tmastant@uwaterloo.ca>
2021-02-10 17:33:27 +0000

    removed digest part

--- db/migrate/20210208012207_create_users.rb
+++ db/migrate/20210208012207_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :watiam, null: false, limit: 50
       t.integer :user_id, unique: true
-      t.string :password_digest, null: false, limit: 100
+      t.string :password, null: false, limit: 100
       t.string :first_name, null: false, limit: 50
       t.string :last_name, null: false, limit: 50
 
