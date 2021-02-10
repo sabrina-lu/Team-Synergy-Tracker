8142c2611f722cb0031c67996d8f03a3fce6a5b6
Thomas Mastantuono <tmastant@uwaterloo.ca>
2021-02-10 17:28:52 +0000

    added password_digest to schema and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2021_02_09_042930) do
   create_table "users", force: :cascade do |t|
     t.string "watiam", limit: 50, null: false
     t.integer "user_id"
-    t.string "password", limit: 100, null: false
+    t.string "password_digest", limit: 100, null: false
     t.string "first_name", limit: 50, null: false
     t.string "last_name", limit: 50, null: false
     t.datetime "created_at", precision: 6, null: false
