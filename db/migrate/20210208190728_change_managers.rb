9b543fdb1afe6e33a3c92599bf918dbfc7c61de4
Laurie Gao <m46gao@uwaterloo.ca>
2021-02-11 00:51:07 +0000

    display teams on user dashboard

--- db/migrate/20210208190728_change_managers.rb
+++
@@ -1,8 +0,0 @@
-class ChangeManagers < ActiveRecord::Migration[6.0]
-  def change
-      remove_index :managers, :watiam
-      add_column(:managers, :manager_id, :integer, null: false, serial: true)
-      add_column(:managers, :password, :string, null: false)
-      add_index(:managers, :manager_id, unique: true)
-  end
-end
