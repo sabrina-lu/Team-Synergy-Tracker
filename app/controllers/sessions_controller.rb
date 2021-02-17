9072df08c9828e6cdb72b664025b71bec9f50645
Sabrina Lu <s248lu@uwaterloo.ca>
2021-02-17 00:26:03 +0000

    fix box url error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root_url, notice: 'Logged out.'
+    redirect_to login_path, notice: 'Logged out.'
   end
 end
