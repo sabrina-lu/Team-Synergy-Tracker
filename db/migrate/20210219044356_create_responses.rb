a58a015dee1073e36226db09adc16354734a1b8b
Emma Lin <e33lin@uwaterloo.ca>
2021-02-21 08:23:37 +0000

    trying to figure out correct params

--- db/migrate/20210219044356_create_responses.rb
+++ db/migrate/20210219044356_create_responses.rb
@@ -4,7 +4,7 @@ class CreateResponses < ActiveRecord::Migration[6.0]
       t.references :survey, null: false, foreign_key: true
       t.integer :question_number, null:false
       t.integer :response, null:false
-        
+      
       t.timestamps
     end
   end
