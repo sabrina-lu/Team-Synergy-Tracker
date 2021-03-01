8ea8db177e6b6787e81d9445ecd1cfd6a66ccf53
Laurie Gao <m46gao@uwaterloo.ca>
2021-03-01 03:58:10 +0000

    fix survey indicator bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,10 @@ class UsersController < ApplicationController
   def dashboard
      @user = current_user #get logged in user from session
      @teams = @user.teams
-     @surveys = @user.surveys
      @completed = {}
      @teams.each do |team| #for each team user is on, check if user completed the survey for that team
-         if @surveys.exists?(team_id: team.id)
+         @survey = @user.surveys.where(team_id: team.id).last
+         if @survey and @survey.responses.exists?
              @completed[team] = true
          else 
              @completed[team] = false
