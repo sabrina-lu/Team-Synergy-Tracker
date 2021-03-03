3185c2e7056f928afe0081e9a79309269e7bc07f
Francois Barnard <fbarnard@uwaterloo.ca>
2021-03-03 05:22:08 +0000

    Add automated testing

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -2,8 +2,19 @@ require 'test_helper'
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @ticket = tickets(:one)
+    setup_tickets
   end
+  
+  test "Should successfully create a ticket with each type of type, priority and category" do 
+    type_options = ["Conflict", "Positive", "Neutral"]
+    category_options = ["Work", "Personal", "Other"]
+    priority_options = [1,2,3]
+    
+    type_options.each do |option1|
+      category_options.each do |option2|
+        priority_options.each do |option3|
+          @t_test = Ticket.create(user_id: @user_1.id, priority: option3, type: option1, category: option2, date: "10/02/2020", description: "d")
+      
 
   test "should get index" do
     get tickets_url
