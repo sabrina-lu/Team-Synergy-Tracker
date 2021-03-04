c3c937daa311f1c22cc50209e62d34f0eab34b95
Francois Barnard <fbarnard@uwaterloo.ca>
2021-03-04 20:52:36 +0000

    Add validation to make sure the description is not empty

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,7 @@
 class Ticket < ApplicationRecord
   self.inheritance_column = nil  #override the inheritance so that type can be used as a column name
   has_and_belongs_to_many :users
-  belongs_to :creator, class_name: "User"
+
+  belongs_to :creator, class_name: "User"  
+  validates :description, presence: true, length: {minimum: 1}
 end
