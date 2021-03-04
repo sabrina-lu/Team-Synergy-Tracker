5f342bbdd833d11c23e2b59b358e2f97475843f9
Francois Barnard <fbarnard@uwaterloo.ca>
2021-03-04 21:47:59 +0000

    Add testing features and some validation for tickets - Add setup for models testing - Add testing coverage for creation - Add testing for controllers - Add size and type validations for tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,9 @@ class Ticket < ApplicationRecord
   self.inheritance_column = nil  #override the inheritance so that type can be used as a column name
   has_and_belongs_to_many :users
 
-  belongs_to :creator, class_name: "User"  
-  validates :description, presence: true, length: {minimum: 1}
+  belongs_to :creator, class_name: "User"
+  validates_length_of :description, maximum: 500, minimum: 1, allow_blank: false
+  validates_inclusion_of :priority, :in => [1, 2, 3]
+  validates_inclusion_of :type, :in => ["Conflict", "Positive", "Neutral"]
+  validates_inclusion_of :category, :in => ['Personal', 'Work', 'Other']
 end
