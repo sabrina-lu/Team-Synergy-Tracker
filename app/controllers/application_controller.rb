5945dd2ffb2a973d15274c3178cc17ae7b962553
Sabrina Lu <s248lu@uwaterloo.ca>
2021-03-18 01:11:42 +0000

    changed typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
     
-    CURRENT_SURVEY_DUE_DATE = Survey.get_current_survey_due_date(Date.new(2021,3,30))
+    CURRENT_SURVEY_DUE_DATE = Survey.get_current_survey_due_date(Date.today)
 end
