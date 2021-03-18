a7c2562ada9d2f5c8895e55d88ddd7f02c1e8b98
Francois Barnard <fbarnard@uwaterloo.ca>
2021-03-18 18:24:27 +0000

    Update models and references to removed user_id column - Remove reference to "user_id" from validations - Remove reference to "user_id" from controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
     validates :first_name, presence: true, allow_blank: false
     validates :last_name, presence: true, allow_blank: false
     validates :watiam, format: {without: /\s/}, presence: true, allow_blank: false
-    validates :user_id, length: {is: 8}, format: {without: /\s/}, presence: true, allow_blank: false
     validates :password, length: {in: 6..25}, on: :create, format: {without: /\s/}, allow_blank: false, presence: true
 
     def full_name_with_watiam
