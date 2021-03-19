2e913d59e7afee826e2a025a026f3497f5ded336
Sabrina Lu <s248lu@uwaterloo.ca>
2021-03-19 01:17:49 +0000

    add ratio to manager dashboard

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,15 @@ class Team < ApplicationRecord
     has_many :surveys
 
     validates :name, length: {in: 1..50}, presence: true, allow_blank: false
+    
+    def get_survey_completion_ratio(current_weekly_survey_due_date)
+       completed_surveys = 0
+       users.each do |user|
+         survey = Survey.find_by(user_id: user.id, team_id: id, date: current_weekly_survey_due_date)
+         if survey && survey.responses.exists?
+           completed_surveys += 1
+         end
+       end
+       return "#{completed_surveys}/#{users.count}"
+    end
 end
