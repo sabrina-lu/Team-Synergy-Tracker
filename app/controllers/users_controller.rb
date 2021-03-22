41a8ce0ab0c5f31dc994a8749255133654e9292f
Sabrina Lu <s248lu@uwaterloo.ca>
2021-03-22 14:02:23 +0000

    fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
           end
           @users = @team.users
           @manager = @team.managers.first  #for now only one manager per team
-          @health_value  = @team.get_total_team_health(0,CURRENT_SURVEY_DUE_DATE)
+          @health_value = 60 #TODO: Get current week's health
       end
   end
     
