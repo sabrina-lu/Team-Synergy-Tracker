de9e0db0948c91987f62d31e35045aecb9289d0f
Emma Lin <e33lin@uwaterloo.ca>
2021-03-23 04:23:39 +0000

    added tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -54,6 +54,45 @@ class ActiveSupport::TestCase
     @t_4.users << [@user_3]
     @t_5.users << [@user_2] 
   end
+    
+  def setup_tickets_with_responses
+    @manager_1 = Manager.create(watiam: "jsmith", first_name: "John", last_name: "Smith", password: "Password")
+    @manager_2 = Manager.create(watiam: "gpaul", first_name: "George", last_name: "Paul", password: "Password")
+      
+    @user_1 = User.create(watiam: "u1", first_name: "user1", last_name: "one", password: "Password")
+    @user_2 = User.create(watiam: "u2", first_name: "user2", last_name: "two", password: "Password")
+    @user_3 = User.create(watiam: "u3", first_name: "user3", last_name: "three", password: "Password")
+    @user_4 = User.create(watiam: "u4", first_name: "user4", last_name: "four", password: "Password")
+    
+    @team_1 = Team.create(name: "one")
+    @team_2 = Team.create(name: "two")
+     
+    @team_1.users << [@user_1, @user_2, @user_3]
+    @team_2.users << [@user_2, @user_3, @user_4]
+    @team_1.managers << [@manager_1]
+    @team_2.managers << [@manager_2]
+   
+    @t_1 = Ticket.create(creator_id: @user_1.id, date:"13/03/2021", team_id: @team_1.id)
+    @t_2 = Ticket.create(creator_id: @user_1.id, date:"14/03/2021", team_id: @team_1.id)
+    @t_3 = Ticket.create(creator_id: @user_2.id, date:"15/03/2021", team_id: @team_2.id)    
+    @t_4 = Ticket.create(creator_id: @user_4.id, date: "19/03/2021", team_id: @team_2.id)
+    @t_5 = Ticket.create(creator_id: @user_4.id, date: "20/03/2021", team_id: @team_2.id)
+      
+    t = [@t_1, @t_2, @t_3, @t_4, @t_5]
+    for i in 0..4
+        TicketResponse.create(ticket_id: t[i].id, question_number: 1, answer: 1)
+        TicketResponse.create(ticket_id: t[i].id, question_number: 2, answer: 2)
+        TicketResponse.create(ticket_id: t[i].id, question_number: 3, answer: 3)
+        TicketResponse.create(ticket_id: t[i].id, question_number: 4, answer: 2)
+        TicketResponse.create(ticket_id: t[i].id, question_number: 5, answer: 7)
+    end
+    
+    @t_1.users << [@user_3]
+    @t_2.users << [@user_2]
+    @t_3.users << [@user_3]   
+    @t_4.users << [@user_3]
+    @t_5.users << [@user_2] 
+  end
   
   def setup_user_for_tickets_models
     @user_4 = User.create(watiam: "u4", first_name: "user4", last_name: "four", password: "Password")
