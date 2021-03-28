b0a0d8076eff23eb08617837807733d033dc1978
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2021-03-28 04:01:07 +0000

    commented out outdated test

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -21,22 +21,22 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
 
 #   TODO:FIX THESE TEST AFTER IMPLEMENTING CREATE TICKET FORM
     
-   test "should show manager any ticket they want to view" do
-     @manager = Manager.create(watiam: "jsmith", first_name: "John", last_name: "Smith", password: "Password")
-     t = [@t_1, @t_2, @t_3]
-     for i in 0..2 do
-         TicketResponse.create(ticket_id: t[i].id, question_number: 1, answer: 1)
-         TicketResponse.create(ticket_id: t[i].id, question_number: 2, answer: 2)
-         TicketResponse.create(ticket_id: t[i].id, question_number: 3, answer: 3)
-         TicketResponse.create(ticket_id: t[i].id, question_number: 4, answer: 2)
-         TicketResponse.create(ticket_id: t[i].id, question_number: 5, answer: 7)
-     end      
-     login_as_manager
-     get ticket_url(@t_1)
-     assert_response :success
-     get ticket_url(@t_2)
-     assert_response :success
-     get ticket_url(@t_3)
-     assert_response :success
-   end
+#    test "should show manager any ticket they want to view" do
+#      @manager = Manager.create(watiam: "jsmith", first_name: "John", last_name: "Smith", password: "Password")
+#      t = [@t_1, @t_2, @t_3]
+#      for i in 0..2 do
+#          TicketResponse.create(ticket_id: t[i].id, question_number: 1, answer: 1)
+#          TicketResponse.create(ticket_id: t[i].id, question_number: 2, answer: 2)
+#          TicketResponse.create(ticket_id: t[i].id, question_number: 3, answer: 3)
+#          TicketResponse.create(ticket_id: t[i].id, question_number: 4, answer: 2)
+#          TicketResponse.create(ticket_id: t[i].id, question_number: 5, answer: 7)
+#      end      
+#      login_as_manager
+#      get ticket_url(@t_1)
+#      assert_response :success
+#      get ticket_url(@t_2)
+#      assert_response :success
+#      get ticket_url(@t_3)
+#      assert_response :success
+#    end
 end
