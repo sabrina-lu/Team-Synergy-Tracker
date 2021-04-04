e0186f0ea6e06661926b47e786bf7f869a54ad04
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2021-04-04 06:31:52 +0000

    Adding and editing controller tests

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -26,7 +26,7 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
         assert_redirected_to user_dashboard_url
     end
     
-    test "should show manager any ticket they want to view" do
+    test "should not show manager tickets that aren't for their team" do
       @manager = Manager.create(watiam: "jsmith", first_name: "John", last_name: "Smith", password: "Password")
       t = [@t_1, @t_2, @t_3]
       for i in 0..2 do
@@ -36,6 +36,42 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
           TicketResponse.create(ticket_id: t[i].id, question_number: 4, answer: 2)
           TicketResponse.create(ticket_id: t[i].id, question_number: 5, answer: 7)
       end      
+      login_as_manager
+      get ticket_url(@t_1)
+      assert_redirected_to manager_dashboard_url
+      get ticket_url(@t_2)
+      assert_redirected_to manager_dashboard_url
+      get ticket_url(@t_3)
+      assert_redirected_to manager_dashboard_url
+    end
+    
+    test "should show manager tickets that are for their team" do
+      @manager = Manager.create(watiam: "jsmith", first_name: "John", last_name: "Smith", password: "Password")
+      @team = Team.create(name: "test")
+      @team.managers << @manager
+        
+      @t_1.team = @team  
+      @t_2.team = @team
+      @t_3.team = @team
+        
+      TicketResponse.create(ticket_id: @t_1.id, question_number: 1, answer: 1)
+      TicketResponse.create(ticket_id: @t_1.id, question_number: 2, answer: 2)
+      TicketResponse.create(ticket_id: @t_1.id, question_number: 3, answer: 3)
+      TicketResponse.create(ticket_id: @t_1.id, question_number: 4, answer: 2)
+      TicketResponse.create(ticket_id: @t_1.id, question_number: 5, answer: 7)
+        
+      TicketResponse.create(ticket_id: @t_2.id, question_number: 1, answer: 1)
+      TicketResponse.create(ticket_id: @t_2.id, question_number: 2, answer: 2)
+      TicketResponse.create(ticket_id: @t_2.id, question_number: 3, answer: 3)
+      TicketResponse.create(ticket_id: @t_2.id, question_number: 4, answer: 2)
+      TicketResponse.create(ticket_id: @t_2.id, question_number: 5, answer: 7)
+      
+      TicketResponse.create(ticket_id: @t_3.id, question_number: 1, answer: 1)
+      TicketResponse.create(ticket_id: @t_3.id, question_number: 2, answer: 2)
+      TicketResponse.create(ticket_id: @t_3.id, question_number: 3, answer: 3)
+      TicketResponse.create(ticket_id: @t_3.id, question_number: 4, answer: 2)
+      TicketResponse.create(ticket_id: @t_3.id, question_number: 5, answer: 7)
+        
       login_as_manager
       get ticket_url(@t_1)
       assert_response :success
