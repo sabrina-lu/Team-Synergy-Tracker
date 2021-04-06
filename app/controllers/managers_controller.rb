57ead273b7e04e938158812a105f5bf6a66b4463
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2021-04-06 06:48:04 +0000

    Added messages if no completed surveys or if no survey history

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -36,6 +36,13 @@ class ManagersController < ApplicationController
       @team_users = @team.users
       @users = User.get_ordered_survey_indicator(@team, CURRENT_SURVEY_DUE_DATE) 
       @team_health_history = @team.get_health_history(CURRENT_SURVEY_DUE_DATE) 
+      
+      if Response.where(survey_id: Survey.where(team_id: @team.id)).exists?
+        @any_team_health_history = true
+      else
+        @any_team_health_history = false
+      end
+        
       if @team_health_history.present?
         @health_value = @team_health_history[0][6] 
       else 
